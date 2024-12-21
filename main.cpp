820551873801b7fc01525188fce691f16c34afb7
Cytezera <ezrachin05@gmail.com>
2024-12-21 22:23:59 +0800

    Finished the traffic light with multithread

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ struct TrafficLightColour{
 	string colour; 
 	int timer; 
 };
+int globalTimer = 1; 
 class TrafficLight{ 
 public: 
 	TrafficLightColour red = {"RED",5};
@@ -20,28 +21,44 @@ public:
 	TrafficLight(): currentLight{ &red, &green, &yellow}, currentIndex(0) {} ; //Allowing it to acess each struct address through numbers 
 	void displayLight(const string inter){
 		cout << inter << "Light : " << currentLight[currentIndex]->colour  << endl; 
-		this_thread::sleep_for(chrono::seconds(currentLight[currentIndex]->timer)); 
 		
+	}
+	void waitLight(){
+
+		this_thread::sleep_for(chrono::seconds(currentLight[currentIndex]->timer)); 
 	}
 
 	void changeLight(){
 		
 		currentIndex = (currentIndex + 1) % 3 ; 
 	}
+	void pauseLight(string inter){
+		cout << inter << "Light : RED" << endl; 
+		this_thread::sleep_for(chrono::seconds(2)); 
+	}
 	void trafficLightTimer(const string inter , int startingIndex){
 		currentIndex = startingIndex;
 		while(true){
 			
-			displayLight(inter); 
+			if (currentIndex != 0) {
+				displayLight(inter); 
+			}
+			waitLight(); 
+			if (currentIndex != 1) {
+
+				pauseLight(inter); 
+			}
 			changeLight(); 
 		}
 	}	
+
 
 };
 int main (){
 	TrafficLight light1; 
 	TrafficLight light2; 
 	vector<thread> threads; 
+	
        	threads.emplace_back(&TrafficLight::trafficLightTimer, &light1 , "Intersection 1 ", 0  ) ; 
 	threads.emplace_back(&TrafficLight::trafficLightTimer, &light2, "Intersection 2" , 1 ) ; 	
 	for (auto& thread: threads){
